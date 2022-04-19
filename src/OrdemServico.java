eb6e72f56f3ec8b166c77f5f044231bd5b238e11
edrsjunior <edilsonrsjunior@gmail.com>
2022-04-19 12:10:24 -0300

    create OrdemServico

---
+++ src/OrdemServico.java
@@ -0,0 +1,63 @@
+import java.time.LocalDateTime;
+
+public class OrdemServico {
+    private int numeroOS;
+    private LocalDateTime dataOS;
+    private LocalDateTime dataPrevTermino;
+    private LocalDateTime dataTermino;
+    private String placaCarro;
+    private String situacao;
+
+    public int getNumeroOS() {
+        return numeroOS;
+    }
+    public void setNumeroOS(int numeroOS) {
+        this.numeroOS = numeroOS;
+    }
+    public LocalDateTime getDataOS() {
+        return dataOS;
+    }
+    public void setDataOS(LocalDateTime dataOS) {
+        this.dataOS = dataOS;
+    }
+    public LocalDateTime getDataPrevTermino() {
+        return dataPrevTermino;
+    }
+    public void setDataPrevTermino(LocalDateTime dataPrevTermino) {
+        this.dataPrevTermino = dataPrevTermino;
+    }
+    public LocalDateTime getDataTermino() {
+        return dataTermino;
+    }
+    public void setDataTermino(LocalDateTime dataTermino) {
+        this.dataTermino = dataTermino;
+    }
+    public String getPlacaCarro() {
+        return placaCarro;
+    }
+    public void setPlacaCarro(String placaCarro) {
+        this.placaCarro = placaCarro;
+    }
+    public String getSituacao() {
+        return situacao;
+    }
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
+    
+    @Override
+    public String toString() {
+        return "OrdemServico" 
+                +"========================"
+                +"[Data da Ordem=" + dataOS.toString() + "\n" 
+                +"Previsão Termino=" + dataPrevTermino +"\n"
+                + "Termino="+ dataTermino +"\n"
+                + "OS ID=" + numeroOS + "\n"
+                +"placaCarro=" + placaCarro + "\n" 
+                +"situacao=" + situacao
+                + "]";
+    }
+
+    
+    
+}
