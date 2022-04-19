d267163acb3896407d8898bc7fe428c94147d544
edrsjunior <edilsonrsjunior@gmail.com>
2022-04-19 13:22:02 -0300

    create Pecas

---
+++ src/Peca.java
@@ -0,0 +1,33 @@
+public class Peca {
+    private int codPeca;
+    private String descricao;
+    private float preco;
+    private int qtdeEstoque;
+    public int getCodPeca() {
+        return codPeca;
+    }
+    public void setCodPeca(int codPeca) {
+        this.codPeca = codPeca;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public float getPreco() {
+        return preco;
+    }
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+    public int getQtdeEstoque() {
+        return qtdeEstoque;
+    }
+    public void setQtdeEstoque(int qtdeEstoque) {
+        this.qtdeEstoque = qtdeEstoque;
+    }
+
+    
+
+}
