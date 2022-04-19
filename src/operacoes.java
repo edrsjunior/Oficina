7c389cf302486d5ddf2651023bf601be483ad94d
edrsjunior <edilsonrsjunior@gmail.com>
2022-04-19 17:12:54 -0300

    all menus

--- src/operacoes.java
+++ src/operacoes.java
@@ -14,4 +14,61 @@ public class operacoes {
 
         return Integer.parseInt(option);
     }
+
+    public static int gerenciarClient() {
+        String message = 
+        "1 - Cadastrar \n"
+        +"2 - Consultar por CPF \n"
+        +"3 - Excluir \n"
+        +"4 - Editar \n"
+        +"5 - Listar todos os cadastros \n"
+        +"6 - Voltar \n";
+
+        String option = JOptionPane.showInputDialog(null, message);
+
+        return Integer.parseInt(option);
+    }
+
+    public static int gerenciarPecasORServices() {
+        String message = 
+        "1 - Cadastrar \n"
+        +"2 - Consultar por código \n"
+        +"3 - Excluir \n"
+        +"4 - Editar \n"
+        +"5 - Listar todos os cadastros \n"
+        +"6 - Voltar \n";
+
+        String option = JOptionPane.showInputDialog(null, message);
+
+        return Integer.parseInt(option);
+    }
+
+    
+    public static int gerenciarOS() {
+        String message = 
+        "1 - Abrir nova ordem de serviço \n"
+        +"2 - Gerenciar Itens \n"
+        +"3 - Excluir \n"
+        +"4 - Editar \n"
+        +"5 - Listar todos as ordens \n"
+        +"6 - Voltar \n";
+
+        String option = JOptionPane.showInputDialog(null, message);
+
+        return Integer.parseInt(option);
+    }
+
+    public static int gerenciarItens() {
+        String message = 
+        "1 - Adicionar nova peça \n"
+        +"2 - Adicionar serviço \n"
+        +"3 - Excluir peça\n"
+        +"4 - Excluir Serviço \n"
+        +"5 - Consultar Total \n"
+        +"6 - Voltar \n";
+
+        String option = JOptionPane.showInputDialog(null, message);
+
+        return Integer.parseInt(option);
+    }
 }
